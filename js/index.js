a0ebc876482d0c8d717ac6da7e49984e53522c84
joaquingutierrez <joaquinguty@gmail.com>
2024-02-21 12:22:59 -0300

    Trabajando en el dropdown de la tabla de servicios 2

--- js/index.js
+++ js/index.js
@@ -7,11 +7,14 @@ const verMasButton = (column) => {
     const tableGraficaExtraContent = document.getElementById("table-grafica-extraContent")
     const tableDiseñoWebExtraContent = document.getElementById("table-diseñoWeb-extraContent")
     const tableDiseñoGraficoExtraContent = document.getElementById("table-diseñoGrafico-extraContent")
+    const tableArrow = document.querySelectorAll(".table-arrow")
     switch (column) {
         case 0:
             if (tableGraficaExtraContent.classList.contains("table-shownContent")) {
                 tableGraficaExtraContent.classList.remove("table-shownContent")
                 tableGraficaButton.parentElement.style.borderBottom = "1px solid #ffffff1a"
+                tableArrow[0].classList.add("table-arrow-down")
+                tableArrow[0].classList.remove("table-arrow-up")
             } else {
                 tableGraficaButton.parentElement.style.borderBottom = "none"
                 tableDiseñoWebButton.parentElement.style.borderBottom = "1px solid #ffffff1a"
@@ -19,12 +22,15 @@ const verMasButton = (column) => {
                 tableGraficaExtraContent.classList.add("table-shownContent")
                 tableDiseñoWebExtraContent.classList.remove("table-shownContent")
                 tableDiseñoGraficoExtraContent.classList.remove("table-shownContent")
+                tableArrow[0].classList.add("table-arrow-up")
+                tableArrow[0].classList.remove("table-arrow-down")
             }
             break
         case 1:
             if (tableDiseñoWebExtraContent.classList.contains("table-shownContent")) {
                 tableDiseñoWebExtraContent.classList.remove("table-shownContent")
                 tableDiseñoWebButton.parentElement.style.borderBottom = "1px solid #ffffff1a"
+                tableArrow[1].classList.remove("table-arrow-up")
             } else {
                 tableGraficaButton.parentElement.style.borderBottom = "1px solid #ffffff1a"
                 tableDiseñoWebButton.parentElement.style.borderBottom = "none"
@@ -32,12 +38,15 @@ const verMasButton = (column) => {
                 tableGraficaExtraContent.classList.remove("table-shownContent")
                 tableDiseñoWebExtraContent.classList.add("table-shownContent")
                 tableDiseñoGraficoExtraContent.classList.remove("table-shownContent")
+                tableArrow[1].classList.add("table-arrow-up")
+                tableArrow[1].classList.remove("table-arrow-down")
             }
             break
         case 2:
             if (tableDiseñoGraficoExtraContent.classList.contains("table-shownContent")) {
                 tableDiseñoGraficoExtraContent.classList.remove("table-shownContent")
                 tableDiseñoGraficoButton.parentElement.style.borderBottom = "1px solid #ffffff1a"
+                tableArrow[2].classList.remove("table-arrow-up")
             } else {
                 tableGraficaButton.parentElement.style.borderBottom = "1px solid #ffffff1a"
                 tableDiseñoWebButton.parentElement.style.borderBottom = "1px solid #ffffff1a"
@@ -45,6 +54,8 @@ const verMasButton = (column) => {
                 tableGraficaExtraContent.classList.remove("table-shownContent")
                 tableDiseñoWebExtraContent.classList.remove("table-shownContent")
                 tableDiseñoGraficoExtraContent.classList.add("table-shownContent")
+                tableArrow[2].classList.add("table-arrow-up")
+                tableArrow[2].classList.remove("table-arrow-down")
             }
             break
     }
