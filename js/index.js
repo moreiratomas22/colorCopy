4cf3014186cc4543df28033ecfebee57db2be612
joaquingutierrez <joaquinguty@gmail.com>
2024-02-21 18:40:05 -0300

    Estilando el footer, arreglando algunos problemas con el dropdown y agregando enlaces

--- js/index.js
+++ js/index.js
@@ -3,58 +3,63 @@ const tableDiseñoWebButton = document.getElementById("table-diseñoWeb-button")
 const tableDiseñoGraficoButton = document.getElementById("table-diseñoGrafico-button")
 
 const verMasButton = (column) => {
-    const table = document.querySelector(".servicios-table")
-    const tableGraficaExtraContent = document.getElementById("table-grafica-extraContent")
-    const tableDiseñoWebExtraContent = document.getElementById("table-diseñoWeb-extraContent")
-    const tableDiseñoGraficoExtraContent = document.getElementById("table-diseñoGrafico-extraContent")
+    const tableExtraContent = document.getElementById("table-extraContent").children
     const tableArrow = document.querySelectorAll(".table-arrow")
     switch (column) {
         case 0:
-            if (tableGraficaExtraContent.classList.contains("table-shownContent")) {
-                tableGraficaExtraContent.classList.remove("table-shownContent")
-                tableGraficaButton.parentElement.style.borderBottom = "1px solid #ffffff1a"
+            if (tableExtraContent[0].classList.contains("table-shownContent")) {
+                tableExtraContent[0].classList.remove("table-shownContent")
                 tableArrow[0].classList.add("table-arrow-down")
                 tableArrow[0].classList.remove("table-arrow-up")
+                tableGraficaButton.parentElement.style.borderBottom = "1px solid #ffffff1a"
             } else {
                 tableGraficaButton.parentElement.style.borderBottom = "none"
                 tableDiseñoWebButton.parentElement.style.borderBottom = "1px solid #ffffff1a"
                 tableDiseñoGraficoButton.parentElement.style.borderBottom = "1px solid #ffffff1a"
-                tableGraficaExtraContent.classList.add("table-shownContent")
-                tableDiseñoWebExtraContent.classList.remove("table-shownContent")
-                tableDiseñoGraficoExtraContent.classList.remove("table-shownContent")
+                tableExtraContent[0].classList.add("table-shownContent")
+                tableExtraContent[1].classList.remove("table-shownContent")
+                tableExtraContent[2].classList.remove("table-shownContent")
                 tableArrow[0].classList.add("table-arrow-up")
                 tableArrow[0].classList.remove("table-arrow-down")
+                tableArrow[1].classList.remove("table-arrow-up")
+                tableArrow[2].classList.remove("table-arrow-up")
             }
             break
         case 1:
-            if (tableDiseñoWebExtraContent.classList.contains("table-shownContent")) {
-                tableDiseñoWebExtraContent.classList.remove("table-shownContent")
-                tableDiseñoWebButton.parentElement.style.borderBottom = "1px solid #ffffff1a"
+            if (tableExtraContent[1].classList.contains("table-shownContent")) {
+                tableExtraContent[1].classList.remove("table-shownContent")
+                tableArrow[1].classList.add("table-arrow-down")
                 tableArrow[1].classList.remove("table-arrow-up")
+                tableDiseñoWebButton.parentElement.style.borderBottom = "1px solid #ffffff1a"
             } else {
                 tableGraficaButton.parentElement.style.borderBottom = "1px solid #ffffff1a"
                 tableDiseñoWebButton.parentElement.style.borderBottom = "none"
                 tableDiseñoGraficoButton.parentElement.style.borderBottom = "1px solid #ffffff1a"
-                tableGraficaExtraContent.classList.remove("table-shownContent")
-                tableDiseñoWebExtraContent.classList.add("table-shownContent")
-                tableDiseñoGraficoExtraContent.classList.remove("table-shownContent")
+                tableExtraContent[0].classList.remove("table-shownContent")
+                tableExtraContent[1].classList.add("table-shownContent")
+                tableExtraContent[2].classList.remove("table-shownContent")
                 tableArrow[1].classList.add("table-arrow-up")
+                tableArrow[0].classList.remove("table-arrow-up")
                 tableArrow[1].classList.remove("table-arrow-down")
+                tableArrow[2].classList.remove("table-arrow-up")
             }
             break
         case 2:
-            if (tableDiseñoGraficoExtraContent.classList.contains("table-shownContent")) {
-                tableDiseñoGraficoExtraContent.classList.remove("table-shownContent")
-                tableDiseñoGraficoButton.parentElement.style.borderBottom = "1px solid #ffffff1a"
+            if (tableExtraContent[2].classList.contains("table-shownContent")) {
+                tableExtraContent[2].classList.remove("table-shownContent")
+                tableArrow[2].classList.add("table-arrow-down")
                 tableArrow[2].classList.remove("table-arrow-up")
+                tableDiseñoGraficoButton.parentElement.style.borderBottom = "1px solid #ffffff1a"
             } else {
                 tableGraficaButton.parentElement.style.borderBottom = "1px solid #ffffff1a"
                 tableDiseñoWebButton.parentElement.style.borderBottom = "1px solid #ffffff1a"
                 tableDiseñoGraficoButton.parentElement.style.borderBottom = "none"
-                tableGraficaExtraContent.classList.remove("table-shownContent")
-                tableDiseñoWebExtraContent.classList.remove("table-shownContent")
-                tableDiseñoGraficoExtraContent.classList.add("table-shownContent")
+                tableExtraContent[0].classList.remove("table-shownContent")
+                tableExtraContent[1].classList.remove("table-shownContent")
+                tableExtraContent[2].classList.add("table-shownContent")
                 tableArrow[2].classList.add("table-arrow-up")
+                tableArrow[0].classList.remove("table-arrow-up")
+                tableArrow[1].classList.remove("table-arrow-up")
                 tableArrow[2].classList.remove("table-arrow-down")
             }
             break
