5f5c6c1ae7489e5c8d8fcd4782f34507c5862396
joaquingutierrez <joaquinguty@gmail.com>
2024-04-09 15:33:31 -0300

    Mejoras en la galeria

--- js/gallery.js
+++ js/gallery.js
@@ -1,18 +1,12 @@
-window.addEventListener('resize', () => {
-    adjustImagePositions();
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-    adjustImagePositions();
-});
-
-
 document.getElementById("prevImgButton").addEventListener("click", () => prevImage(currentImageIndex))
 document.getElementById("nextImgButton").addEventListener("click", () => nextImage(currentImageIndex))
 document.getElementById("closeImgButton").addEventListener("click", () => closeGallery())
+const gallery = document.querySelector('#gallery');
+const imageContainers = gallery.querySelectorAll('.imageContainer');
 
 let imageArray = []
 let currentImageIndex = 0
+const totalImages = imageContainers.length
 
 const arrayMinElementPosition = (array) => {
     let min = array[0]
@@ -38,41 +32,43 @@ const arrayMaxElementPosition = (array) => {
     return index
 }
 
-function adjustImagePositions() {
-    const gallery = document.querySelector('#gallery');
-    const images = gallery.querySelectorAll('.imageContainer');
+async function adjustImagePositions() {
     const gap = 5
-    
     const windowWidth = gallery.offsetWidth
     const width = 280 //Todas las columnas son iguales y las imagenes tienen el ancho de la columna
     const columns = Math.floor(windowWidth / width) || 1
     const whiteSpace = windowWidth - columns * width  //espacio desperdiciado
-    
+
     const topColumn = Array.from({ length: columns }, () => 30) //Inicializo las columnas en 30px, para luego ir sumando por columna
     const leftColumn = Array.from({ length: columns }, (_, column) => ((column * width) + (whiteSpace / 2) + column * gap)) //Calculo el inicio de cada columna
-    
+
     let column = 0 //desde 0 hasta columns - 1
-    
-    images.forEach((img, index) => {
-        img.style.width = `${width}px`
-        img.style.height = `${img.children[0].offsetHeight}px`
+
+    for (let i = 0; i < totalImages; i++) {
+        imageContainers[i].firstElementChild.style.left = "0"
+        imageContainers[i].firstElementChild.style.position = "relative"
+        imageContainers[i].style.width = `${width}px`
+
+        const imageHeight = await loadImage(imageContainers[i].firstElementChild)
+        imageContainers[i].style.height = `${imageHeight}px`
+
         const left = leftColumn[column]
         const top = topColumn[column]
-        img.style.visibility = "visible"
-        
-        topColumn[column] += img.children[0].offsetHeight + gap
-        
-        //Ahora tengo que elegir cúal va a ser la siguiente columna donde se ponga la img
+        topColumn[column] += imageContainers[i].children[0].height + gap
+
+        //Ahora tengo que elegir cúal va a ser la siguiente columna donde se ponga la imageContainers[i]
         column = arrayMinElementPosition(topColumn)
 
-        img.style.left = `${left}px`;
-        img.style.top = `${top}px`;
-        img.addEventListener("click", () => openCarrousel(index))
-        imageArray.push(img)
-    });
+        imageContainers[i].style.left = `${left}px`;
+        imageContainers[i].style.top = `${top}px`;
+        imageContainers[i].addEventListener("click", () => openCarrousel(i))
+        imageArray.push(imageContainers[i].children[0])
+        const classesToAdd = ["wow", "animate__animated", "animate__fadeInUp"]
+        imageContainers[i].classList.add(...classesToAdd)
+    }
     const maxCol = arrayMaxElementPosition(topColumn)
     gallery.style.height = `${topColumn[maxCol]}px`
-    window.onclick = function(event) {
+    window.onclick = function (event) {
         const modal = document.getElementById('galleryModal');
         if (event.target === modal) {
             closeGallery();
@@ -82,25 +78,60 @@ function adjustImagePositions() {
 
 const openCarrousel = (index) => {
     const carrousel = document.getElementById("galleryModal")
-    const currentImage = document.getElementById("modalImage")
     currentImageIndex = index
     carrousel.style.display = "block"
-    currentImage.src = imageArray[index].children[0].src
+    const clonedImage = imageArray[index].cloneNode(true)
+    clonedImage.style.left = ""
+    clonedImage.style.display = ""
+    carrousel.appendChild(clonedImage).classList.add("modal-content")
     document.body.style.overflow = "hidden" //Bloqueo el scroll
 }
 const closeGallery = () => {
+    const carrousel = document.getElementById("galleryModal")
     document.getElementById('galleryModal').style.display = 'none';
     document.body.style.overflow = "visible" //desbloqueo el scroll
+    carrousel.removeChild(carrousel.children[3])
 }
 
 const prevImage = (index) => {
+    const carrousel = document.getElementById("galleryModal")
     currentImageIndex = index - 1
     if (currentImageIndex === 0) currentImageIndex = imageArray.length - 1
-    document.getElementById('modalImage').src = imageArray[currentImageIndex].children[0].src;
+    carrousel.removeChild(carrousel.children[3])
+    const clonedImage = imageArray[currentImageIndex].cloneNode(true)
+    clonedImage.style.left = ""
+    clonedImage.style.display = ""
+    carrousel.appendChild(clonedImage).classList.add("modal-content")
 }
 
 const nextImage = (index) => {
+    const carrousel = document.getElementById("galleryModal")
     currentImageIndex = index + 1
     if (currentImageIndex === imageArray.length) currentImageIndex = 0
-    document.getElementById('modalImage').src = imageArray[currentImageIndex].children[0].src;
+    carrousel.removeChild(carrousel.children[3])
+    const clonedImage = imageArray[currentImageIndex].cloneNode(true)
+    clonedImage.style.left = ""
+    clonedImage.style.display = ""
+    carrousel.appendChild(clonedImage).classList.add("modal-content")
+}
+
+window.addEventListener("load", () => {
+    adjustImagePositions();
+    const spinner = document.getElementById("loader")
+    spinner.style.display = "none"
+    window.addEventListener('resize', () => {
+        adjustImagePositions();
+    });
+})
+
+const loadImage = (image) => {
+    return new Promise((res, rej) => {
+        if (image.complete) {
+            res(image.height)
+        } else {
+            image.onload = () => {
+                res(image.height)
+            }
+        }
+    })
 }
